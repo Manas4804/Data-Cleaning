c411a68853eb771219a7de864d6ee911d1c61b67
Manas4804 <Manaschintawar10@gmail.com>
2026-06-14 23:43:31 +0530

    Data Cleaning Project 1

---
+++ Data_Cleaning.sql
@@ -0,0 +1,221 @@
+select * from layoffs;
+
+-- 1. Remove Duplicate
+-- 2. Standardize the Data
+-- 3. Null values or Blank Values
+-- 4. Remove any comlums
+
+-- 1. Remove Duplicates
+
+create table layoffs_staging
+like layoffs;
+
+
+select * 
+from layoffs_staging;
+
+insert layoffs_staging
+select * 
+from layoffs;
+
+select * from layoffs_staging;
+
+SELECT *
+FROM world_layoffs.layoffs_staging
+;
+
+SELECT company, industry, total_laid_off,`date`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, industry, total_laid_off,`date`) AS row_num
+	FROM 
+		world_layoffs.layoffs_staging;
+
+
+SELECT *
+FROM (
+	SELECT company, industry, total_laid_off,`date`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, industry, total_laid_off,`date`
+			) AS row_num
+	FROM 
+		world_layoffs.layoffs_staging
+) duplicates
+WHERE 
+	row_num > 1;
+    
+    SELECT *
+FROM world_layoffs.layoffs_staging
+WHERE company = 'Oda'
+;
+
+
+SELECT *
+FROM (
+	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		world_layoffs.layoffs_staging
+) duplicates
+WHERE 
+	row_num > 1;
+  
+
+ALTER TABLE world_layoffs.layoffs_staging ADD row_num INT;
+
+CREATE TABLE `world_layoffs`.`layoffs_staging2` (
+`company` text,
+`location`text,
+`industry`text,
+`total_laid_off` text,
+`percentage_laid_off` text,
+`date` text,
+`stage`text,
+`country` text,
+`funds_raised_millions` text,
+row_num int
+);
+
+INSERT INTO `world_layoffs`.`layoffs_staging2`
+(`company`,
+`location`,
+`industry`,
+`total_laid_off`,
+`percentage_laid_off`,
+`date`,
+`stage`,
+`country`,
+`funds_raised_millions`,
+`row_num`)
+SELECT `company`,
+`location`,
+`industry`,
+`total_laid_off`,
+`percentage_laid_off`,
+`date`,
+`stage`,
+`country`,
+`funds_raised_millions`,
+		ROW_NUMBER() OVER (
+			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
+			) AS row_num
+	FROM 
+		world_layoffs.layoffs_staging;
+        
+        
+select * from layoffs_staging2
+where row_num > 1;
+
+
+delete from 
+layoffs_staging2
+where row_num > 1;
+
+
+-- 2. Standardizing data
+
+select company, trim(company)
+from layoffs_staging2;
+
+update layoffs_staging2
+set company = trim(company);
+
+select distinct industry 
+from layoffs_staging2
+order by 1;
+
+select *
+from layoffs_staging2
+where industry like 'crypto%';
+
+update layoffs_staging2
+set industry = 'Crypto'
+where industry like 'Crypto%';
+
+select distinct country , trim(trailing '.' from country) 
+from layoffs_staging2
+order by 1;
+
+update layoffs_staging2
+set country = trim(trailing '.' from country)
+where country like 'United States%';
+
+select `date`
+from layoffs_staging2;
+
+update layoffs_staging2
+set `date` = str_to_date(`date`, '%m/%d/%Y');
+
+alter table layoffs_staging2
+modify column `date` Date;
+
+SELECT 
+    *
+FROM
+    layoffs_staging2
+WHERE
+    total_laid_off IS NULL
+        AND percentage_laid_off IS NULL;
+        
+        
+update layoffs_staging2
+set industry = null
+where industry = '';        
+        
+
+
+
+select t1.industry, t2.industry
+from layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company  = t2.company
+where t1.industry is null 
+and t2.industry is not null;
+
+update layoffs_staging2 t1
+join layoffs_staging2 t2
+	on t1.company = t2.company
+set t1.industry = t2.industry
+where t1.industry is null
+and t2.industry is not null;
+
+
+select *
+from layoffs_staging2
+where industry is null;
+
+SELECT 
+    *
+FROM
+    layoffs_staging2
+WHERE
+    total_laid_off IS NULL
+        AND percentage_laid_off IS NULL;
+
+delete
+FROM
+    layoffs_staging2
+WHERE
+    total_laid_off IS NULL
+        AND percentage_laid_off IS NULL;
+
+
+select *
+from layoffs_staging2;
+
+alter table layoffs_staging2
+drop column row_num;
+
+
+
+
+
+
+
+
+
+
+
+
+
